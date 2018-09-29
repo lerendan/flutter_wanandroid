ae5b978c484583143edf3cfa342c1b9dd37dacab
lerendan <340212100@qq.com>
2018-09-29 16:55:13 +0800

    修复navigationbar切换item后重复build的问题

--- lib/index/index.dart
+++ lib/index/index.dart
@@ -7,111 +7,119 @@ class Index extends StatefulWidget {
 }
 
 class _IndexState extends State<Index> {
+
+  final navigationBarTitles = ['首页', '项目','体系', '我的'];
+
   final _bottomNavigationColor = Colors.blue;
   int _currentIndex = 0;
   List<Widget> list = List();
 
   @override
   void initState() {
-    list
-      ..add(new HomePage())
-      ..add(new Center(child: new Text("1"),))
-      ..add(new Center(child: new Text("2"),))
-      ..add(new Center(child: new Text("3"),));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white70,
-      appBar: AppBar(
-        title: Text('WAN ANDROID'),
-      ),
-      body: list[_currentIndex],
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the Drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        fixedColor: Colors.green,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                color: _bottomNavigationColor,
-              ),
-              title: Text(
-                '首页',
-                style: TextStyle(color: _bottomNavigationColor),
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.email,
-                color: _bottomNavigationColor,
-              ),
-              title: Text(
-                'Email',
-                style: TextStyle(color: _bottomNavigationColor),
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.pages,
-                color: _bottomNavigationColor,
-              ),
-              title: Text(
-                'PAGES',
-                style: TextStyle(color: _bottomNavigationColor),
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.face,
-                color: _bottomNavigationColor,
-              ),
-              title: Text(
-                '我的',
-                style: TextStyle(color: _bottomNavigationColor),
-              )),
-        ],
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        type: BottomNavigationBarType.shifting,
-      ),
+
+    final _body = new IndexedStack(
+      children: <Widget>[
+        new HomePage(),
+        new Center(child: new Text("1")),
+        new Center(child: new Text("2")),
+        new Center(child: new Text("3"))
+      ],
+      index: _currentIndex,
     );
+
+    return Scaffold(
+        backgroundColor: Colors.white70,
+        appBar: AppBar(
+          title: Text('WAN ANDROID'),
+        ),
+        body: _body,
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the Drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: _bottomNavigationColor,
+                ),
+                title: Text(
+                  navigationBarTitles[0],
+                  style: TextStyle(color: _bottomNavigationColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.email,
+                  color: _bottomNavigationColor,
+                ),
+                title: Text(
+                  navigationBarTitles[1],
+                  style: TextStyle(color: _bottomNavigationColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.pages,
+                  color: _bottomNavigationColor,
+                ),
+                title: Text(
+                  navigationBarTitles[2],
+                  style: TextStyle(color: _bottomNavigationColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: _bottomNavigationColor,
+                ),
+                title: Text(
+                  navigationBarTitles[3],
+                  style: TextStyle(color: _bottomNavigationColor),
+                )),
+          ],
+          currentIndex: _currentIndex,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.shifting,
+        ),
+      );
   }
 }
